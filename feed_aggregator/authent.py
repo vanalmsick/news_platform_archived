526f6997c09f45d5fb83cd2030240b4e6fdf1f41
vanalmsick <44527602+vanalmsick@users.noreply.github.com>
2023-08-16 23:47:05 +0100

    Firewall working version. #minor

--- feed_aggregator/authent.py
+++ feed_aggregator/authent.py
@@ -14,13 +14,17 @@ def get_article_data(pk):
         article = requested_article.__dict__
         article['full_text'] = mark_safe(article['full_text'])
         article['publisher__name'] = requested_article.publisher.name
+        if len(article['full_text']) > 30 and '<img ' in article['full_text'][:30]:
+            article['image_url'] = ''
+        if len(article['summary']) > 30 and article['summary'][:30] in article['full_text']:
+            article['summary'] = ''
         article.pop('_state')
         SHARE_EMAIL_SUBJECT = f"{article['publisher__name']}: {article['title']}"
         SHARE_EMAIL_BODY = f"""Hi,\n\nHave you seen this article:\n\n{SHARE_EMAIL_SUBJECT}\n{article['link']}\n\nBest wishes,\n\n"""
         SHARE_TEAMS_LINK = f"""{SHARE_EMAIL_SUBJECT}\n{article['link']}"""
         article[f'email__link'] = 'mailto:?subject=' + urllib.parse.quote(
             SHARE_EMAIL_SUBJECT) + '&body=' + urllib.parse.quote(SHARE_EMAIL_BODY)
-        article[f'teams__link'] = 'https://teams.microsoft.com/l/chat/0/0?message=' + urllib.parse.quote(
+        article[f'teams__link'] = 'https://teams.microsoft.com/l/chat/0/0?users=sven.van.almsick@morganstanley.com&message=' + urllib.parse.quote(
             SHARE_TEAMS_LINK)
         article['error'] = False
     except:
