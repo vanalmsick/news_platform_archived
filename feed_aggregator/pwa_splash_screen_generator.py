27e9339014b41834b3d1dd3dcf483c4baf08c6ba
vanalmsick <44527602+vanalmsick@users.noreply.github.com>
2024-02-24 22:15:41 +0000

    Fix meta tags of PWA Splash Screens. #patch

--- feed_aggregator/pwa_splash_screen_generator.py
+++ feed_aggregator/pwa_splash_screen_generator.py
@@ -4,24 +4,21 @@ import os
 from PIL import Image
 
 IPONE_SCREENS = [
-    dict(width=393, height=852, scale=3),  # 14 & 15 Pro
-    dict(width=430, height=932, scale=3),  # 14 & 15 Max
-    dict(width=428, height=926, scale=3),  # 12 & 13 & 14 Plus
-    dict(width=390, height=844, scale=3),  # 12 & 13 & 14
-    dict(width=375, height=812, scale=3),  # 12 & 13 Mini
-    dict(width=414, height=896, scale=3),  # X & 12 Max
-    dict(width=375, height=812, scale=3),  # X & 11
-    dict(width=414, height=896, scale=2),
-    dict(width=414, height=896, scale=2),
-    dict(width=414, height=896, scale=3),
-    dict(width=375, height=812, scale=3),
-    dict(width=414, height=736, scale=3),
-    dict(width=375, height=667, scale=2),
     dict(width=1024, height=1366, scale=2),
-    dict(width=834, height=1112, scale=2),
-    dict(width=320, height=568, scale=2),
     dict(width=834, height=1194, scale=2),
+    dict(width=834, height=1112, scale=2),
+    dict(width=810, height=1080, scale=2),
     dict(width=768, height=1024, scale=2),
+    dict(width=430, height=932, scale=3),
+    dict(width=428, height=926, scale=3),
+    dict(width=414, height=896, scale=3),
+    dict(width=414, height=896, scale=2),
+    dict(width=414, height=736, scale=3),
+    dict(width=393, height=852, scale=3),
+    dict(width=390, height=844, scale=3),
+    dict(width=375, height=812, scale=3),
+    dict(width=375, height=667, scale=2),
+    dict(width=320, height=568, scale=2),
 ]
 
 
@@ -31,20 +28,22 @@ def create():
         width = props["width"]
         height = props["height"]
         scale = props["scale"]
-        file_name = f"./static/splashscreens/splash_{width}_{height}_{scale}.png"
 
-        if os.path.isfile(file_name) is False:
-            # Create a blank image with white background
-            image = Image.new("RGB", (width, height), "white")
+        for w, h in [(width, height), (height, width)]:
+            file_name = f"./static/splashscreens/splash_{w}_{h}_{scale}.png"
 
-            logo = Image.open("static/logo.png", "r")
+            if os.path.isfile(file_name) is False:
+                # Create a blank image with white background
+                image = Image.new("RGB", (w * scale, h * scale), "white")
 
-            # Calculate the x and y coordinates to align the text in the middle
-            x = (image.width - logo.width) // 2
-            y = (image.height - logo.height) // 2 - logo.height // 2
+                logo = Image.open("static/logo.png", "r")
 
-            # Draw the text in the middle
-            image.paste(logo, (x, y), logo)
+                # Calculate the x and y coordinates to align the text in the middle
+                x = (image.width - logo.width) // 2
+                y = (image.height - logo.height) // 2 - logo.height // 2
 
-            # Save the image
-            image.save(file_name)
+                # Draw the text in the middle
+                image.paste(logo, (x, y), logo)
+
+                # Save the image
+                image.save(file_name)
